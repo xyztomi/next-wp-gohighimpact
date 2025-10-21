a2a1893559a028dac668aeb358d63244ea3a9e08
xyztomi <amandipthandi@gmail.com>
2025-10-21 23:55:56 +0700

    chore: set runtime to edge for API route

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,6 +1,8 @@
 import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
 
+export const runtime = "edge";
+
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = new URL(request.url);
