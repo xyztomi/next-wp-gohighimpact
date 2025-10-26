66290f90d55f612c9d862e39632d4f6394e00141
xyztomi <amandipthandi@gmail.com>
2025-10-26 12:17:02 +0700

    feat: update keywords in metadata and add Facebook Pixel tracking script for enhanced analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export const metadata: Metadata = {
     images: [`${siteConfig.site_domain}/hero-video-thumbnail.svg`],
   },
   keywords: [
-    "gohighlevel crm guide",
+    "gohighlevel crm",
     "gohighlevel 2025",
     "marketing automation platform",
     "white label crm software",
@@ -80,8 +80,31 @@ export default async function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '758973117142664');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className={cn("min-h-screen font-sans antialiased overflow-x-hidden overflow-y-scroll no-scrollbar", font.variable)}>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=758973117142664&ev=PageView&noscript=1"
+            alt="facebook pixel"
+          />
+        </noscript>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-23HWYQVF1Z" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
