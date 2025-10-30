3444cfa8cee6559d6ff2c72e586145ac7257216b
xyztomi <amandipthandi@gmail.com>
2025-10-31 00:07:40 +0700

    fix: update implementation guidance text in TrialPopup component

--- components/trial-popup.tsx
+++ components/trial-popup.tsx
@@ -119,7 +119,7 @@ export function TrialPopup() {
                 <svg className="mt-0.5 h-5 w-5 shrink-0 text-brand-green" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                 </svg>
-                <span>Done-for-you implementation guidance</span>
+                <span>Done-with-you implementation guidance</span>
               </li>
             </ul>
 
